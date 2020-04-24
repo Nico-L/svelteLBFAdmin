f95dcc56c9cd1ca2595f467f8c27da3ced85f641
Nico-L <luchier@gmail.com>
2020-04-24 21:47:53 +0000

    fix navigation

--- src/layouts/navigation.svelte
+++ src/layouts/navigation.svelte
@@ -1,9 +1,9 @@
 <script>
-    import { Link, links } from "svelte-routing";
+    import { Link } from "svelte-routing";
 </script>
 
 <h4 class="text-orangeLBF text-center mb-2">L'atelier</h4>
-<div class="my-2 pl-2 cursor-pointer hover:bg-vertLBFT hover:text-gray-200 text-vertLBF"><a href="/">Retour à l'accueil</a></div>
+<div class="my-2 pl-2 cursor-pointer hover:bg-vertLBFT hover:text-gray-200 text-vertLBF"><Link to="/">Retour à l'accueil</Link></div>
 <h5 class="ml-2">Les ateliers</h5>
 <ul class="ml-2">
     <Link to="/ateliers/agenda"><li class="pl-2 cursor-pointer hover:bg-orangeLBFT">Agenda des ateliers</li></Link>
@@ -12,12 +12,11 @@
 <hr class="ml-2 my-3 w-5/6 border-orangeLBFT"/>
 <h5 class="ml-2 w-5/6 ">Les machines</h5>
 <ul class="ml-2 mb-2 navi">
-    <a href="/machines/agendaReservations"><li class="pl-2 cursor-pointer hover:bg-orangeLBFT">Agenda des réservations</li></a>
+    <Link to="/machines/agendaReservations"><li class="pl-2 cursor-pointer hover:bg-orangeLBFT">Agenda des réservations</li></Link>
     <Link to="/machines/listeMachines"><li class="pl-2 cursor-pointer hover:bg-orangeLBFT">Liste des machines</li></Link>
     <Link to="/machines/nouvelleMachine"><li class="pl-2 cursor-pointer hover:bg-orangeLBFT">Nouvelle fiche machine</li></Link>
     <Link to="/machines/abonnements"><li class="pl-2 cursor-pointer hover:bg-orangeLBFT">Abonnements</li></Link>
-    <Link to="/machines/plagesReservations"><li class="pl-2 cursor-pointer hover:bg-orangeLBFT">Horaires de réservation</li></Link>
-    
+    <Link to="/machines/plagesReservations"><li class="pl-2 cursor-pointer hover:bg-orangeLBFT">Horaires de réservation</li></Link>   
 </ul>
 
 
