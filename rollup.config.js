5fcc2e09caeff53a3ed522b9051f1797c3e00c5b
Nico-L <nicolas@luchier.fr>
2023-05-13 13:07:08 +0000

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,8 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-//import livereload from 'rollup-plugin-livereload';
 import browsersync from 'rollup-plugin-browsersync';
-import { terser } from 'rollup-plugin-terser';
+import {terser} from 'rollup-plugin-terser';
 import historyApiFallback from 'connect-history-api-fallback'
 import replace from '@rollup/plugin-replace';
 import css from 'rollup-plugin-css-only'
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -279,8 +279,8 @@ module.exports = {
     minWidth: ["responsive"],
     backgroundOpacity: ['hover']
   },
-  plugins: [
-        require('tailwind-scrollbar'),
-        require('@tailwindcss/line-clamp')
-    ]
+ /* plugins: [
+        //require('tailwind-scrollbar'),
+        //require('@tailwindcss/line-clamp')
+    ] */
 };
